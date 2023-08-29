d76e3e48f50984fcdb55d705c435a072a3af6124
Aymane11 <aymaneboumaaza@gmail.com>
2023-08-29 13:45:44 +0100

    update slug form validation

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,11 +1,21 @@
 from django import forms
+from django.core.validators import RegexValidator
 from django.forms.widgets import TextInput
+
+validate_slug = RegexValidator(
+    regex=r"^[a-zA-Z0-9]+$",
+    message="Invalid Slug (Letters and numbers only)",
+    code="invalid_slug",
+)
 
 
 class ShortForm(forms.Form):
     website = forms.URLField(max_length=200, widget=TextInput, required=True)
-    slug = forms.SlugField(
-        label="Slug", required=False, help_text="Optional (Letters and numbers only)"
+    slug = forms.CharField(
+        label="Slug",
+        required=False,
+        help_text="Optional (Letters and numbers only)",
+        validators=[validate_slug],
     )
 
     def __init__(self, *args, **kwargs):
